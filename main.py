57bb9226704758cf04e44aab7c04faea4e1825e6
JJ Shep <jjshep42ac+github@gmail.com>
2024-09-28 13:43:54 -0400

    Create specific objectives in main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Take name of book as input
+# Convert spaces to underscores
+# Append converted name of book to "en.wikipedia.org/wiki/"
+# Check if article exists on Wikipedia. If not, try appending "_(novel)" to the string and try again
+# Otherwise say not found
+# Get book properties from fields on the article's infobox card
